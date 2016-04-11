59deeb73a2eacdc919c45e379a3a000227f57136
Dave Strauss <dave@expeditelabs.com>
2016-04-10 22:46:38 -0400

    split computation and render

--- graph-color/constraints.py
+++ graph-color/constraints.py
@@ -16,6 +16,7 @@ what type of 'inference algorithm' do we use?
 import graphs
 from pyrsistent import PClass, field, s
 import random
+import time
 
 class Graph(PClass):
     edges = field()
@@ -33,28 +34,43 @@ class Graph(PClass):
 
         return valid_assignment
 
-def backtrack(nodes, problem):
-    if all([y.color for y in nodes]):
-        # all nodes have an assignment
-        return nodes
+class BacktrackSolver():
+    def __init__(self):
+        self.start_time = 0
+        self.stop_time = 0
+        self.backtrack_calls = 0
+        self.constraint_tests = 0
+        self.failed_node_choice = 0
 
-    node_view = random.choice([y for y in nodes if not y.color])
-    remaining_domain = node_view.domain
+    def external_backtrack(self, problem):
+        self.start_time = time.time()
+        soln = self.backtrack(problem)
+        self.stop_time = time.time()
+        self.runtime = self.stop_time - self.start_time
+        return soln
 
-    constraints = problem.get_constraint_func(node_view.id)
+    def backtrack(self, problem):
+        self.backtrack_calls += 1
+        if all([y.color for y in problem.nodes.values()]):
+            # all nodes have an assignment
+            return problem
 
-    for val in remaining_domain:
-        if constraints(val):
-            # put val in the color
-            new_node = node_view.set(color=val)
-            new_nodes = [node for node in nodes if node.id != new_node.id] + [new_node]
-            new_problem = problem.set(nodes=dict([(node.id, node) for node in new_nodes]))
-            status = backtrack(new_nodes, new_problem)
-            if status:
-                return status
-        else:
-            print("failed constraint", val)
-    print("failed all constraints", remaining_domain)
+        node_view = random.choice([y for y in problem.nodes.values() if not y.color])
+        remaining_domain = node_view.domain
+
+        constraints = problem.get_constraint_func(node_view.id)
+
+        for val in remaining_domain:
+            if constraints(val):
+                self.constraint_tests += 1
+                # put val in the color
+                new_node = node_view.set(color=val)
+                new_nodes = [node for node in problem.nodes.values() if node.id != new_node.id] + [new_node]
+                new_problem = problem.set(nodes=dict([(node.id, node) for node in new_nodes]))
+                status = self.backtrack(new_problem)
+                if status:
+                    return status
+        self.failed_node_choice += 1
 
 class Node(PClass):
     id = field(int)
